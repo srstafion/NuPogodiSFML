994ca1b13a0069e60a072f85fe6bccf8e0f58a68
Nazaretyan Agasi <nazaretyanaghasi@gmail.com>
2024-03-18 18:09:46 +0300

    player can now catch eggs

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+﻿#include "Game.h"
 #include <iostream>
 
 void Game::processEvents()
@@ -24,22 +24,18 @@ void Game::processEvents()
         if (Keyboard::isKeyPressed(Keyboard::D)) {
             buttons[0].changeStatus();
             wolfPos = 0;
-            cout << wolfPos;
         }
         else if (Keyboard::isKeyPressed(Keyboard::C)) {
             buttons[1].changeStatus();
             wolfPos = 1;
-            cout << wolfPos;
         }
         else if (Keyboard::isKeyPressed(Keyboard::K)) {
             buttons[2].changeStatus();
             wolfPos = 2;
-            cout << wolfPos;
         }
         else if (Keyboard::isKeyPressed(Keyboard::M)) {
             buttons[3].changeStatus();
             wolfPos = 3;
-            cout << wolfPos;
         }
     }
 }
@@ -49,20 +45,26 @@ void Game::render()
     window.clear();
     window.draw(frame);
     window.draw(screen);
-    for (auto i : buttons) {
-        window.draw(i.getShape());
+
+    for (auto el : buttons) {
+        window.draw(el.getShape());
     }
+
     for (int i = 0, count = 0; i < 4; i++) {
         for (int j = 0; j < 6; j++) {
-            if (chickens[i][j] == true) window.draw(eggs[count]);
+            if (chickens[i][j]) {
+                window.draw(eggs[count]);
+            }
+            count++;
         }
-        count++;
     }
+    window.draw(score);
     window.display();
 }
 
 void Game::update()
 {
+    score.setString(to_string(caughtEggs));
     switch (wolfPos) {
     case 0:
         screenT.loadFromFile("./Textures/LU.png");
@@ -81,14 +83,28 @@ void Game::update()
         screen.setTexture(&screenT);
         break;
     }
-    if (value % spawnRate == 0) {
+    if (value >= speed) {
         spawnCount++;
         moveEggs();
         if (spawnCount >= spawnRate) {
             spawnCount = 0;
             createRandomEgg();
         }
-        showInfoEggs();
+        value = 0;
+        /*showInfoEggs();*/
+        if ((caughtEggs % (10 * level)) == 0 && caughtEggs > 0) {
+            if (spawnRate > 1) {
+                spawnRate--;
+                level++;
+                cout << spawnRate << " : " << level << endl;
+            }
+        }
+        if ((caughtEggs % (20 * level)) == 0 && caughtEggs > 0) {
+            if (speed > 4) speed--;
+        }
+        if (mistakes == 3) {
+            window.close();
+        }
     }
 }
 
@@ -101,56 +117,87 @@ void Game::incrementer()
 }
 
 Game::Game()
-    :window(VideoMode(1200, 690), "Nu Pogodi!"),
+    : window(sf::VideoMode(1200, 690), "Nu Pogodi!"),
     th(incrementer)
 {
-    screen.setSize(Vector2f(650, 420));
-    screenT.loadFromFile("./Textures/LU.png");
-    screen.setTexture(&screenT);
-    screen.setPosition(275, 130);
-    frame.setSize(Vector2f(1200, 690));
+    scoreF.loadFromFile("Segment7Standard.otf");
+    score.setFont(scoreF);
+    score.setPosition(650, 140);
+    score.setCharacterSize(70);
+    score.setFillColor(Color::Black);
+
     frameT.loadFromFile("./Textures/frame.png");
+    frame.setSize(sf::Vector2f(1200, 690));
     frame.setTexture(&frameT);
-    buttons.push_back(Button(Vector2f(90, 440)));
-    buttons.push_back(Button(Vector2f(90, 560)));
-    buttons.push_back(Button(Vector2f(1045, 440)));
-    buttons.push_back(Button(Vector2f(1045, 560)));
+
+    screenT.loadFromFile("./Textures/lu.png");
+    screen.setSize(sf::Vector2f(637, 410));
+    screen.setPosition(sf::Vector2f(280, 138));
+    screen.setTexture(&screenT);
+
+    sf::Vector2f pos1(95, 440);
+    sf::Vector2f pos2(95, 565);
+    sf::Vector2f pos3(1045, 440);
+    sf::Vector2f pos4(1045, 565);
+
+    buttons.push_back(Button(pos1));
+    buttons.push_back(Button(pos2));
+    buttons.push_back(Button(pos3));
+    buttons.push_back(Button(pos4));
 
     eggT.loadFromFile("./Textures/egg.png");
-    int posEggs[24][3] = { { 330, 265, 20 },
-                           { 370, 275, 80 },
-                           { 400, 300, 140 },
-                           { 420, 325, 180 },
-                           { 440, 350, 220 },
-                           { 450, 510, 200 },
 
-                           { 330, 365, 20 },
-                           { 370, 375, 80 },
-                           { 400, 400, 140 },
-                           { 420, 425, 180 },
-                           { 440, 450, 220 },
-                           { 450, 510, 200 }, 
+    int posEggs[24][3] = { {330, 265, 20},
+                            {370, 275, 80},
+                            {400, 300, 140},
+                            {420, 325, 180},
+                            {440, 350, 220},
+                            {450, 510, 200}, // Óïàëî
 
-                           { 870, 293, 200 },
-                           { 854, 290, 140 },
-                           { 820, 288, 80 },
-                           { 780, 302, 20 },
-                           { 745, 345, -40 },
-                           { 765, 510, 200 },
+                            {330, 365, 20},
+                            {370, 375, 80},
+                            {400, 400, 140},
+                            {420, 425, 180},
+                            {440, 450, 220},
+                            {450, 510, 200}, // Óïàëî
 
-                           { 870, 393, 200 },
-                           { 854, 390, 140 },
-                           { 820, 388, 80 },
-                           { 780, 402, 20 },
-                           { 745, 445, -40 },
-                           { 765, 510, 200 } };
-    for (int i = 0; i < 25; i++) {
-        RectangleShape shape;
-        shape.setSize(Vector2f(26, 19));
+                            {870, 293, 200},
+                            {854, 290, 140},
+                            {820, 288, 80},
+                            {780, 302, 20},
+                            {745, 345, -40},
+                            {765, 510, 200}, // Óïàëî
+
+                            {870, 393, 200},
+                            {854, 390, 140},
+                            {820, 388, 80},
+                            {780, 402, 20},
+                            {745, 445, -40},
+                            {765, 510, 200}, // Óïàëî
+    };
+
+    for (int i = 0; i < 24; i++) {
+        sf::RectangleShape shape;
+
+        shape.setSize(sf::Vector2f(26, 19));
         shape.setTexture(&eggT);
-        shape.setPosition(Vector2f(posEggs[i][0], posEggs[i][1]));
+        shape.setPosition(sf::Vector2f(posEggs[i][0], posEggs[i][1]));
         shape.rotate(posEggs[i][2]);
+
         eggs.push_back(shape);
+    }
+}
+
+void Game::reset()
+{
+    level = 1;
+    speed = 10;
+    spawnRate = 5;
+    spawnCount = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            chickens[i][j] == 0;
+        }
     }
 }
 
@@ -178,8 +225,19 @@ void Game::createRandomEgg()
 void Game::moveEggs()
 {
     for (int i = 0; i < 4; i++) {
-        for (int j = 6; j > 0; j--) {
+        for (int j = 5; j > 0; j--) {
             swap(chickens[i][j], chickens[i][j - 1]);
+            if (j == 5 && chickens[i][j]) {
+                if (wolfPos == i && chickens[i][j] == 1) {
+                    chickens[i][j] = 0;
+                    caughtEggs++;
+                }
+                else if (wolfPos != i) {
+                    mistakes++;
+                    reset();
+                    cout << spawnRate << " : " << level << endl;
+                }
+            }
         }
     }
 }
--- Game.h
+++ Game.h
@@ -12,33 +12,46 @@ class Game
 {
 private:
 	RenderWindow window;
+
 	vector<Button> buttons;
+
 	RectangleShape screen;
 	Texture screenT;
+
 	RectangleShape frame;
 	Texture frameT;
+
 	vector<RectangleShape> eggs;
 	Texture eggT;
-	int mistakes = 0;
+
 	bool chickens[4][6]{ 0 };
+
+	int mistakes = 0;
 	int wolfPos = 0; //0 left up 1 left down 2 right up 3 right down
+
 	const Time TimePerFrame = seconds(1.f / 10.f);
 	thread th;
 	int static value;
+
+	Text score;
+	Font scoreF;
+	int spawnCount = 0;
+
 	int level = 1;
 	int speed = 10;
 	int spawnRate = 5;
-	int spawnCount = 0;
 	int caughtEggs = 0;
 	int coordsEggs[4][6]{ {} };
 
 	void processEvents();
 	void render();
 	void update();
+
 	void static incrementer();
 	void createRandomEgg();
 	void showInfoEggs();
 	void moveEggs();
+	void reset();
 public:
 	Game();
 	~Game();
